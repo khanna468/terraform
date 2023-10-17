02672bea7e6ce772536d6bf6daa090e94ac5dd54
khanna468 <64956647+khanna468@users.noreply.github.com>
2023-10-17 16:26:59 +0530

    Update ec2_useast1.tf

--- ec2_useast1.tf
+++ ec2_useast1.tf
@@ -1,51 +1,51 @@
-resource "aws_instance" "east1" {
-  ami           = lookup(var.ami_map, var.region_map)
-  instance_type = "t3.micro"
-   availability_zone = "us-east-1a"
-   vpc_security_group_ids = [aws_security_group.private_Security_group_east.id]
+#resource "aws_instance" "east1" {
+  #ami           = lookup(var.ami_map, var.region_map)
+  #istance_type = "t3.micro"
+   3availability_zone = "us-east-1a"
+   #vpc_security_group_ids = [aws_security_group.private_Security_group_east.id]
    #key_name = "aaru"
-   provider = aws.us-east-1
-  tags = {
-    Name = "East Instance"
-  }
-}
+   #provider = aws.us-east-1
+  #tags = {
+  #  Name = "East Instance"
+ # }
+#}
 
-data "aws_vpc" "vpc" {
-  provider = aws.us-east-1
-  filter {
-    name = "tag:Name"
-    values = ["vpc"]
-  }
-}
+#data "aws_vpc" "vpc" {
+  #provider = aws.us-east-1
+  #filter {
+   # name = "tag:Name"
+  #  values = ["vpc"]
+ # }
+#}
 
-resource "aws_security_group" "private_Security_group_east" {
-  name        = "private instance SG"
-  description = "Allow inbound traffic"
-  vpc_id      = "${data.aws_vpc.vpc.id}"
+#resource "aws_security_group" "private_Security_group_east" {
+ # name        = "private instance SG"
+  #description = "Allow inbound traffic"
+  #vpc_id      = "${data.aws_vpc.vpc.id}"
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  #ingress {
+    #from_port        = 22
+    #to_port          = 22
+    #protocol         = "tcp"
+   # cidr_blocks      = ["0.0.0.0/0"]
     
-  }
+  #}
 
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+  #ingress {
+    #from_port        = 0
+    #to_port          = 0
+    #protocol         = "-1"
+   # cidr_blocks      = ["0.0.0.0/0"]
     
-  }
+  #}
   
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+  #egress {
+   # from_port        = 0
+  #  to_port          = 0
+ #   protocol         = "-1"
+ #   cidr_blocks      = ["0.0.0.0/0"]
+# }
 
   tags = {
     Name = " private Instance"
