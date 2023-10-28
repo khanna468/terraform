a37f68c361c92ca44e7989b1367abb4d41b5168d
rajesh <khanna.468@gmail.com>
2023-10-28 13:55:47 +0530

    Module export1

--- src/news3/terraform.tf
+++ src/news3/terraform.tf
@@ -1,12 +1,15 @@
 terraform {
   backend "s3" {
-    bucket = module.mys3.bucket_id
+    bucket = local.bucket
     region = "ap-south-1"
     key    = "terraform.tfstate"
     
   }
 }
 
+locals {
+    bucket = module.mys3.bucket_id
+}
 provider "aws" {
     region = "ap-south-1"
   
