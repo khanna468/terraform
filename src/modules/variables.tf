e9ae17b7a5f1f4e4625cffb172d1729546304e73
rajesh <khanna.468@gmail.com>
2023-10-28 13:04:26 +0530

    mosules

---
+++ src/modules/variables.tf
@@ -0,0 +1,20 @@
+variable "bucket_name" {
+    type = string
+  
+}
+
+variable "environment" {
+    type = string
+  
+}
+
+variable "acl" {
+    default = None
+  
+}
+
+variable "region" {
+  type = string
+  default = "ap-south-1"
+}
+
