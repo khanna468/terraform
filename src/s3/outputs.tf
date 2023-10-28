992d95b718f41eb47e2641dd8a7815d678ecaa21
Ubuntu <ubuntu@ip-172-31-32-223.ap-south-1.compute.internal>
2023-10-28 08:18:48 +0000

    New COmmit

---
+++ src/s3/outputs.tf
@@ -0,0 +1,8 @@
+output "bucket_id" {
+   value = module.mys3.id
+}
+
+output "bucket_arn" {
+   value = module.mys3.arn
+}
+
--- src/s3/s3.tf
+++ src/s3/s3.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "mys3" {
-    source = "../../modules/s3"   
+    source = "../modules/s3"   
     bucket_name = "pandugadu-bucket"
     environment = "dev"
-    acl         = private
+    acl         = "private"
     
 
 }
